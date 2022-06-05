183c60aaf88f2f6b00716bc1db35c63626e69c28
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-06-05 18:37:12 -0400

    added test for classes following,post and comments

--- instagram/tests.py
+++ instagram/tests.py
@@ -17,4 +17,43 @@ class ProfileTestClass(TestCase):
         profile = Profile.objects.all()
         self.assertTrue(len(profile) > 0)
 
+class PostTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new post and saving it
+        self.valarie= Profile(name = 'valarie', bio ='Live.Love.Laugh')
+        self.valarie.save_profile()
+
+        # Creating a new comment and saving it
+        self.new_comment = Comment(name = 'testing')
+        self.new_comment.save()
+
+        self.new_post= Post(caption = 'hey there',profile = 'valarie')
+        self.new_post.save()
+
+        self.new_post.comment.add(self.new_comment)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Comment.objects.all().delete()
+        Post.objects.all().delete()
+
+class FollowingTestClass(TestCase):
+    def setUp(self):
+        self.sam=Following(name='sam',followed='valarie')
+                            
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sam,Following))
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.first=Comment(post=1,
+                            name='sam',
+                            comment='nice picture',
+                            date='May 20, 2018, 11:50 a.m.',
+                            count=0)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.first,Comment))
+
   
