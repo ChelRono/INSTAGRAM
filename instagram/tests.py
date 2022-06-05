358efd3fe0e282230595c5377a98a466e9b12fbc
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-06-05 18:11:15 -0400

    put test for profile class

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Profile,Post,Following,Comment
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.valarie= Profile(name = 'valarie', bio ='Live.Love.Laugh')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.valarie,Profile))
+
+        # Testing Save Method
+    def test_save_method(self):
+        self.valarie.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
